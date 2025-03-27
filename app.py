2c0e3aa06a85fcdd18679e00e311770a167ac51d
whips <whips@musinux.com>
2025-03-27 14:46:05 +0100

    updates files

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-
+import subprocess
+import time
+import sys
 import mido
 import scoring.settings
 import scoring.sequence
@@ -7,9 +9,21 @@ from functions.playPattern import play_pattern
 from functions.import_sequence import import_sequence
 from functions.display import *
 
+
 # Main function to load pattern, sequence, and play
 def main():
     try:
+        #launch automatically the FluidSynth instance locally
+        print(f"\n{YELLOW}launching FluidSynth instance !{RESET}\n")
+        synth_process = subprocess.Popen(
+            ["fluidsynth", "-a", "alsa", "-f ", "scoring/FSconfig.txt" , "-m", "alsa_seq",  "/usr/share/sounds/sf2/FluidR3_GM.sf2"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE
+        )
+        # wait a little to ensure midi channel is up ... (reduce if needed)
+        time.sleep(2)
+        print(f"{mido.open_output(mido.get_output_names()[2])}")
         # Default values
         default_signature = "4/4"
         default_tempo = 120
@@ -20,6 +34,8 @@ def main():
         tempo = int(tempo_input) if tempo_input else default_tempo
     except ValueError:
         print(f"{RED}Veuillez entrer des valeurs valides.{RESET}")
+
+
 
     # Open MIDI output port
     try:
@@ -48,18 +64,23 @@ def main():
     hide_cursor()
     # Start an infinite loop to repeat the sequence
     while True:  # Infinite loop to keep repeating the sequence
-        for pattern_ref in pattern_sequence:
-            if pattern_ref in patterns:
-                pattern = patterns[pattern_ref]
-                # Call the function to play the pattern
-                print(f"{BLUE}Bar {YELLOW}{scoring.settings.global_bar_counter}{RESET}:\t\t {BLUE}Pattern {GREEN}{pattern_ref} \tTF : {RED}{scoring.settings.tension_factor}{RESET}", end="\n") #\r to return 
-                play_pattern(pattern, tempo, midi_out, channel=9)
-            else:
-                print(f"{RED}Pattern {pattern_ref} not found in the loaded patterns.{RESET}")
-        
-        # After all patterns in the sequence are played, the loop will restart
-        print(f"{BLUE}Looping the sequence ... {RESET}                   ")
+        try:
+            for pattern_ref in pattern_sequence:
+                if pattern_ref in patterns:
+                    pattern = patterns[pattern_ref]
+                    # Call the function to play the pattern
+                    print(f"{BLUE}Bar {YELLOW}{scoring.settings.global_bar_counter}{RESET}:\t\t {BLUE}Pattern {GREEN}{pattern_ref} \tTF : {RED}{scoring.settings.tension_factor}{RESET}", end="\n") #\r to return 
+                    play_pattern(pattern, tempo, midi_out, channel=9)
+                else:
+                    print(f"{RED}Pattern {pattern_ref} not found in the loaded patterns.{RESET}")
+            
+            # After all patterns in the sequence are played, the loop will restart
+            print(f"{BLUE}Looping the sequence ... {RESET}                   ")
 
+        except KeyboardInterrupt:
+            print(f"\n{RED}Stopping Sequencer, Script & Synth...{RESET}")
+            synth_process.terminate()  # Ensure Qsynth is closed
+            sys.exit(0)  # Exit script
 
 if __name__ == "__main__":
     main()
