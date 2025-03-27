5386f3a455ac11aac137885f5d9e1a3cdb3dce40
whips <whips@musinux.com>
2025-03-27 16:30:21 +0100

    refactoring start files

--- scoring/sequence.py
+++ scoring/sequence.py
@@ -4,4 +4,6 @@
 # '00', '01' ... ar MODES changes
 
 #pattern_sequence = ['W1', 'W1'] # used with 6/8 
-main_sequence = ['A0', '+2', 'A0','+2','A0', '+2','A0', '+2', 'A0', '-8'] # used with test pattern
+#main_sequence = ['A0', '+2', 'A0','+2','A0', '+2','A0', '+2', 'A0', '-8'] # used with test pattern
+
+main_sequence = ['A2', 'A2'] # simple loop of 2x4 mes
